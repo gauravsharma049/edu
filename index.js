1acea6ca66f48258170d276e8c8815402610739b
gauravsharma049 <85776209+gauravsharma049@users.noreply.github.com>
2023-04-23 16:45:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ function navbar(){
     return (
         `<nav class="navbar navbar-expand-md navbar-light bg-warning">
         <div class="container-fluid">
-          <a class="navbar-brand" href="/"><img height="40px" src="/images/logo.png" alt="Logo"></a>
+          <a class="navbar-brand" href="/"><img height="40px" src="images/logo.png" alt="Logo"></a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
